d4f280baf920d08a736dfd0e9818e4b5d1c563bb
James Shelley <jcsdev@icloud.com>
2018-01-01 17:25:44 +0000

    Styling
    Small styling changes

--- js/usd.js
+++ js/usd.js
@@ -1,1 +1,5 @@
- var cf_widget_size = "large"; var cf_widget_from = "DOGE"; var cf_widget_to = "usd"; var cf_widget_name = "Dogecoin"; var cf_clearstyle = true;
+ var cf_widget_size = "large";
+ var cf_widget_from = "DOGE";
+ var cf_widget_to = "usd";
+ var cf_widget_name = "Dogecoin";
+ var cf_clearstyle = true;
